f87bbff2969e055f7561a4bdcd8f799f32b1cd57
Aron Nopanen <aron@aron-nopanens-computer.local>
2010-03-28 22:11:23 +1300

    BaseConnectionTests doesn't have any tests itself

--- src/test/java/cc/wily/logviewer/connections/BaseConnectionTests.java
+++ src/test/java/cc/wily/logviewer/connections/BaseConnectionTests.java
@@ -1,10 +1,7 @@
 package cc.wily.logviewer.connections;
 
-import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/test-app-config.xml")
 public class BaseConnectionTests {
 }
